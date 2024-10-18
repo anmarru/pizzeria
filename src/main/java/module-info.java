dece212eaedd3d10c8792c45ed83b6b9b75a606b
andrea <andreamartinezruiz10@gmail.com>
2024-10-18 13:15:09 +0200

    ok

---
+++ src/main/java/module-info.java
@@ -0,0 +1,21 @@
+module andrea {
+    /*requires javafx.controls;
+    requires javafx.fxml;
+    requires java.xml.bind;
+
+    opens modelo to javafx.fxml;
+    exports modelo;*/
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.xml.bind;
+
+    // Exportaciones y aperturas
+    opens modelo.cliente to java.xml.bind;
+    exports modelo.cliente;
+    opens modelo to javafx.fxml;
+    exports modelo;
+
+    // Exporta y abre el paquete modelo.pedido para permitir acceso reflexivo
+    exports modelo.pedido;
+    opens modelo.pedido to java.xml.bind;
+}
